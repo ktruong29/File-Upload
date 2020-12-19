a63ba6c1c3386dfe04b3cc017bc58c8043854a00
Kien Truong <ktruong29@csu.fullerton.edu>
2020-12-18 17:43:23 -0800

    Added boto3 module

--- pkgs.sh
+++ pkgs.sh
@@ -7,4 +7,5 @@ sudo pip3 install pymysql
 sudo pip3 install mysql-connector
 sudo pip3 install Flask
 sudo pip3 install Flask-WTF
-sudo pip3 install passlib 
+sudo pip3 install passlib
+sudo pip3 install boto3
