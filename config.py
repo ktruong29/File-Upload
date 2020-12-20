db5f8aa5d1daf1ad38370e8f4754832054a349c8
Kien Truong <ktruong29@csu.fullerton.edu>
2020-12-19 22:59:30 -0800

    Added bucketname + instructions to config bucket and aws keys

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 #customhost = <your db endpoint address>
-customhost      = "group-project-454.cxt6a6u8d050.us-east-1.rds.amazonaws.com"
+#Before using boto3, read this https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html#using-boto3
+#Install aws cli first https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
+customhost      = "group-project-454.cxt6a6u8d073.us-east-1.rds.amazonaws.com"
 customuser      = "admin"
 custompass      = "group454"
 customdb        = "PERSON"
+bucketname      = "files-uploaded-project454"
