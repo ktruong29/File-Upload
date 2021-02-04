cd6797fed1edb73c14fef4048b8e12d67dd7ad55
Kien Truong <ktruong29@csu.fullerton.edu>
2021-02-04 13:25:49 -0800

    Added File_name table

--- db/user.sql
+++ db/user.sql
@@ -11,6 +11,13 @@ CREATE TABLE User_info (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE File_name(
+	id INT NOT NULL,
+	filename VARCHAR(100) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (id) REFERENCES User_info(id)
+)
+
 desc User_info;
 select * from User_info;
 
@@ -19,4 +26,3 @@ set SQL_SAFE_UPDATES = 0;
 delete from User_info where username = "project454";
 set SQL_SAFE_UPDATES = 1;
 drop schema PERSON
-
